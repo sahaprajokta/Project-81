59892a01b69dba7ce3fd4d3f8233c5b619c26cdc
sahaprajokta <97291708+sahaprajokta@users.noreply.github.com>
2022-08-10 18:52:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+Canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 1;
+ctx.rect(150, 143, 430, 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 1;
+ctx.arc(250, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.strokeStyle = " blue ";
+ctx.lineWidth = 5;
+ctx.arc(250, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
